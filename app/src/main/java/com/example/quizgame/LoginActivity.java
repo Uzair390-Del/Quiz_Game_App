79db6a841231d17520e10cf535908d994c42c6e8
Uzair390-Del <uzair.zairi321@gmail.com>
2025-05-21 14:26:25 +0500

    firebase

--- app/src/main/java/com/example/quizgame/LoginActivity.java
+++ app/src/main/java/com/example/quizgame/LoginActivity.java
@@ -4,15 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     TextView mail;
@@ -21,7 +29,8 @@ public class LoginActivity extends AppCompatActivity {
     SignInButton signInGoogle;
     TextView signUp;
     TextView forgetPassword;
-
+    ProgressBar progressBar;
+    FirebaseAuth auth=FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,9 +45,15 @@ public class LoginActivity extends AppCompatActivity {
             signInGoogle= findViewById(R.id.buttonLoginGoogleSignIn);
             signUp= findViewById(R.id.textViewLoginSignUp);
             forgetPassword= findViewById(R.id.textViewLoginForgetPassword);
+            progressBar=findViewById(R.id.progressBarSignIn);
             signIn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+
+                    String userName=mail.getText().toString();
+                    String userPassword=password.getText().toString();
+                    signInFirebase(userName,userPassword);
+
 
                 }
             });
@@ -65,5 +80,36 @@ public class LoginActivity extends AppCompatActivity {
 
             return insets;
         });
+    }
+    public void signInFirebase(String userName, String userPassord) {
+        progressBar.setVisibility(View.VISIBLE);
+        signIn.setClickable(false);
+        auth.signInWithEmailAndPassword(userName, userPassord).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(LoginActivity.this, "Sign In Successfull!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Sign In is not Successfull!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user=auth.getCurrentUser();
+        if (user!=null)
+        {
+            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+
+        }
     }
 }
--- app/src/main/java/com/example/quizgame/Sign_Up_Page.java
+++ app/src/main/java/com/example/quizgame/Sign_Up_Page.java
@@ -5,18 +5,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Sign_Up_Page extends AppCompatActivity {
     EditText mail;
     EditText password;
     Button signUp;
     ProgressBar progressBar;
+    FirebaseAuth auth= FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +38,41 @@ public class Sign_Up_Page extends AppCompatActivity {
             password= findViewById(R.id.editTextSignUpPassword);
             signUp= findViewById(R.id.buttonSignUpSignUp);
             progressBar= findViewById(R.id.progressBarSignUp);
+            progressBar.setVisibility(View.INVISIBLE);
             signUp.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    signUp.setClickable(false);
+
+                    String userName=mail.getText().toString();
+                    String userPassword=password.getText().toString();
+                    signUpFirebase(userName,userPassword);
 
                 }
             });
 
             return insets;
         });
+
+    }
+    public void signUpFirebase(String userName, String userPassord){
+        progressBar.setVisibility(View.VISIBLE);
+        auth.createUserWithEmailAndPassword(userName,userPassord).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(Sign_Up_Page.this, "Your Account is Created!", Toast.LENGTH_LONG).show();
+                    finish();
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    Toast.makeText(Sign_Up_Page.this, "There is a problem, Please try again later!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 }
