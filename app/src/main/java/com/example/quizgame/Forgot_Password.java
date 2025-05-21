10272456e17eca9636b4c5a55abc628fe1cc56ff
Uzair390-Del <uzair.zairi321@gmail.com>
2025-05-21 14:56:38 +0500

    forgotPassword activity

--- app/src/main/java/com/example/quizgame/Forgot_Password.java
+++ app/src/main/java/com/example/quizgame/Forgot_Password.java
@@ -1,9 +1,9 @@
 package com.example.quizgame;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,29 +11,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.auth.FirebaseAuth;
+public class Forgot_Password extends AppCompatActivity {
+    EditText mail;
+    Button btnContinue;
 
-public class MainActivity extends AppCompatActivity {
-    TextView signOut;
-    FirebaseAuth auth= FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forgot_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            signOut= findViewById(R.id.textViewSignout);
-            signOut.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    auth.signOut();
-                    Intent i =new Intent(MainActivity.this,LoginActivity.class);
-                    startActivity(i);
-                    finish();
-                }
-            });
+           mail=findViewById(R.id.editTextPasswordAndEmail);
+           btnContinue=findViewById(R.id.buttonPasswordContinue);
+           btnContinue.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+
+               }
+           });
+
             return insets;
         });
     }
